dbf45f45fae0e2274f8208fe4db5cfc4170910c7
curyAilen <cury.ailena@gmail.com>
2023-09-08 15:06:06 -0300

    1er Entregable Ailen Cury

--- Entregable 1/app.js
+++ Entregable 1/app.js
@@ -38,3 +38,25 @@ class ProductManager {
         return productIdFind
     }
 }
+
+const productManager = new ProductManager();
+
+const productAdd1 = productManager.addProduct("Producto 1", "prdocuto agregado en indice 1", 1000, "imagen1.jpg", 1, 5);
+console.log ("Producto agregado: ", productAdd1)
+productManager.addProduct("Producto 2", "prdocuto agregado en indice 2", 3005, "imagen2.jpg", 2, 30);
+console.log("Lista de productos:", productManager.getProduct());
+const productEncontrado = productManager.getProductById(2);
+console.log("Producto encontrado por ID (se busco el indice 2):", productEncontrado);
+
+
+//Lineas de código para ejecutar y validar el funcionamiento de los errores//
+
+//Descomentar linea para validar el funcionamiento de los errores. linea 9//
+//productManager.addProduct("Producto 5", "prdocuto agregado en indice 3", "imagen.jpg", 1, 5);
+
+//Descomentar linea para validar el funcionamiento de los errores. linea 13//
+//productManager.addProduct("Producto 1", "prdocuto agregado en indice 1", 1000, "imagen1.jpg", 1, 5);
+
+//Descomentar linea para validar el funcionamiento de los errores. linea 36//
+//const productEncontrado2 = productManager.getProductById(7);
+//console.log("Producto encontrado por ID (se busco el indice 7)", productEncontrado2);
