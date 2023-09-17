b3c2dc66af75d93f05531c1e4f22d87a5374c51e
curyAilen <cury.ailena@gmail.com>
2023-09-17 20:47:57 -0300

    2do Entregable Ailen Cury

--- Entregable 2/app.js
+++ Entregable 2/app.js
@@ -61,7 +61,7 @@ class ProductManager {
         this.saveProduct();
     }
 }
-////////////////////// T E S T E R ///////////////////////
+////////////////////// T E S T E R /////////////////////// 
 const productManager = new ProductManager();
 const product1 = productManager.addProduct("Producto 1", "prdocuto agregado en indice 1", 1000, "imagen1.jpg", 10, 5);
 const product2 = productManager.addProduct("Producto 2", "prdocuto agregado en indice 2", 1005, "imagen0.jpg", 20, 5);
