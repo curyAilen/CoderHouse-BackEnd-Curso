0770be2589df3844b96f2225fc5bf06467bb85b8
curyAilen <cury.ailena@gmail.com>
2023-09-17 20:43:34 -0300

    2da Entregable Ailen Cury

--- Entregable 2/README.md
+++ Entregable 2/README.md
@@ -13,11 +13,11 @@
     - thumbnail (ruta de imagen)
     - code (código identificador)
     - stock (número de piezas disponibles)
-- Debe tener un método **addProduct** el cual debe recibir un objeto con el formato previamente especificado, asignarle un id autoincrementable y guardarlo en el arreglo (recuerda siempre guardarlo como un array en el archivo).
-- Debe tener un método **getProducts**, el cual debe leer el archivo de productos y devolver todos los productos en formato de arreglo.
-- Debe tener un método **getProductById**, el cual debe recibir un id, y tras leer el archivo, debe buscar el producto con el id especificado y devolverlo en formato objeto
-- Debe tener un método **updateProduct**, el cual debe recibir el id del producto a actualizar, así también como el campo a actualizar (puede ser el objeto completo, como en una DB), y debe actualizar el producto que tenga ese id en el archivo. **NO DEBE BORRARSE SU ID**
-- Debe tener un método **deleteProduct**, el cual debe recibir un id y debe eliminar el producto que tenga ese id en el archivo.
+- Debe tener un método **addProduct** el cual debe recibir un objeto con el formato previamente especificado, asignarle un id autoincrementable y guardarlo en el arreglo (recuerda siempre guardarlo como un array en el archivo). **✓**
+- Debe tener un método **getProducts**, el cual debe leer el archivo de productos y devolver todos los productos en formato de arreglo.**✓**
+- Debe tener un método **getProductById**, el cual debe recibir un id, y tras leer el archivo, debe buscar el producto con el id especificado y devolverlo en formato objeto **✓**
+- Debe tener un método **updateProduct**, el cual debe recibir el id del producto a actualizar, así también como el campo a actualizar (puede ser el objeto completo, como en una DB), y debe actualizar el producto que tenga ese id en el archivo. **NO DEBE BORRARSE SU ID** **✓**
+- Debe tener un método **deleteProduct**, el cual debe recibir un id y debe eliminar el producto que tenga ese id en el archivo. **✓**
 
 
 
--- Entregable 2/app.js
+++ Entregable 2/app.js
@@ -1,63 +1,80 @@
+const fs = require('fs');
 class ProductManager {
-    constructor(){
+    constructor() {
+        this.path = 'Product.json';
         this.products = [];
-        this.autoIncrementId  = 1;
-        this.path;
-    }
-
-    addProduct(title, description, price,thumbnail, code, stock){
-        if(!title || !description || !price || !thumbnail || !code || !stock){
+        this.autoIncrementId = 1;
+    }    
+    addProduct(title, description, price, thumbnail, code, stock) {
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
             throw new Error('Los campos son obligatorios');
         }
-        const productExist =  this.products.find(product =>  product.code === code );
-        if(productExist){
+        const productExist = this.products.find(product => product.code === code);
+        if (productExist) {
             throw new Error('Ya existe un Producto con ese ID')
         }
         const newProduct = {
-            id : this.autoIncrementId++,
             title,
             description,
             price,
             thumbnail,
-            code,
+            code: this.autoIncrementId++,
             stock
         }
-        this.products.push(newProduct)  
-
-        return newProduct
+        this.products.push(newProduct);
+        this.saveProduct();
+        return JSON.parse(fs.readFileSync(this.path, 'utf-8')) || [];
     }
-
+    saveProduct() {
+        fs.writeFileSync(this.path, JSON.stringify(this.products, null, 2), 'utf-8');
+    }    
     getProduct(){
-        return this.products;
-            
+        return fs.readFileSync(this.path, 'utf-8') || [];
     }
-    getProductById(id){
-        const productIdFind = this.products.find(productFind => productFind.id === id);
+    getProductById(code){
+        const data = JSON.parse(fs.readFileSync(this.path, 'utf-8'))
+        const productIdFind = data.find(productFind => productFind.code === code);
         if(!productIdFind){
          return console.error("Not Found");
         }
         return productIdFind
     }
+    updateProduct(code, updateData) {
+        const productIndex = this.products.findIndex(product => product.code === code);
+        if (productIndex === -1) {
+            throw new Error('El producto no fue encontrado.');
+        }
+        updateData.code = this.products[productIndex].code;
+        this.products[productIndex] = {
+            ...this.products[productIndex],
+            ...updateData
+        };
+        this.saveProduct();
+        return this.products[productIndex];
+    }
+    deleteProduct(code) {
+        const productIndex = this.products.findIndex(product => product.code === code);
+        if (productIndex === -1) {
+            throw new Error('El producto no fue encontrado.');
+        }
+        this.products.splice(productIndex, 1);
+        this.saveProduct();
+    }
 }
-
+////////////////////// T E S T E R ///////////////////////
 const productManager = new ProductManager();
-
-const productAdd1 = productManager.addProduct("Producto 1", "prdocuto agregado en indice 1", 1000, "imagen1.jpg", 1, 5);
-console.log ("Producto agregado: ", productAdd1)
-productManager.addProduct("Producto 2", "prdocuto agregado en indice 2", 3005, "imagen2.jpg", 2, 30);
-console.log("Lista de productos:", productManager.getProduct());
-const productEncontrado = productManager.getProductById(2);
-console.log("Producto encontrado por ID (se busco el indice 2):", productEncontrado);
-
-
-//Lineas de código para ejecutar y validar el funcionamiento de los errores//
-
-//Descomentar linea para validar el funcionamiento de los errores. linea 9//
-//productManager.addProduct("Producto 5", "prdocuto agregado en indice 3", "imagen.jpg", 1, 5);
-
-//Descomentar linea para validar el funcionamiento de los errores. linea 13//
-//productManager.addProduct("Producto 1", "prdocuto agregado en indice 1", 1000, "imagen1.jpg", 1, 5);
-
-//Descomentar linea para validar el funcionamiento de los errores. linea 36//
-//const productEncontrado2 = productManager.getProductById(7);
-//console.log("Producto encontrado por ID (se busco el indice 7)", productEncontrado2);
+const product1 = productManager.addProduct("Producto 1", "prdocuto agregado en indice 1", 1000, "imagen1.jpg", 10, 5);
+const product2 = productManager.addProduct("Producto 2", "prdocuto agregado en indice 2", 1005, "imagen0.jpg", 20, 5);
+const product3 = productManager.addProduct("Producto 3", "prdocuto agregado en indice 3", 3050, "imagen12.jpg", 30, 30);
+const product4 = productManager.addProduct("Producto 4", "prdocuto agregado en indice 4", 3005, "imagen12.jpg", 40, 30);
+const product5 = productManager.addProduct("Producto 5", "prdocuto agregado en indice 5", 1000, "imagen1.jpg", 50, 5);
+const product6 = productManager.addProduct("Producto 6", "prdocuto agregado en indice 6", 1077, "imagen0.jpg", 60, 5);
+const product7 = productManager.addProduct("Producto 7", "prdocuto agregado en indice 7", 300, "imagen12.jpg", 70, 30);
+const product8 = productManager.addProduct("Producto 8", "prdocuto agregado en indice 8", 4587, "imagen12.jpg", 80, 30);
+console.log("Productos Agregados", product1,product2, product3)
+const productEncontrado = productManager.getProductById(4);
+console.log("Producto encontrado por ID:", productEncontrado);
+const updateProdut = productManager.updateProduct(2, {title: 'Producto actualizado', description: "Nueva información actulizada y precio", price: 55400});
+console.log('Producto actualizado:', updateProdut);
+productManager.deleteProduct(4);
+console.log('Producto eliminado con ID 4 y traigo todos los productos:', productManager.getProduct());
