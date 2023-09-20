062de027ac8224e8996ca67d131e9862f23acee2
curyAilen <cury.ailena@gmail.com>
2023-09-20 16:24:49 -0300

    quedan rechequear código

--- Entregable 3/app.js
+++ Entregable 3/app.js
@@ -29,7 +29,18 @@ catch(error){
  res.status(500).json({ error: error.message});
 }
 })
+app.get('/products/:pid', async (req, res)=>{
+    try{
+        const productId = parseInt(req.params.pid);
+        await productManager.loadProducts(); 
+        const product = productManager.getProductById(productId);
+        res.json({ product });
+    }
+    catch(error)
+{
+    res.status(404).json({ error: 'Producto no encontrado' });
+}})
 
 app.listen(port, () => {
-    console.log(`Servidor en funcionamiento en el puerto ${port}`);
+    console.log(`Servidor en funcionamiento en el puerto localhost:${port}`);
 });
